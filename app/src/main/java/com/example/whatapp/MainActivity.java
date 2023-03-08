1d2139679f19187483f3be41d67b641310cde91b
MuhammadAnees1 <chanees281@gmail.com>
2023-03-08 12:55:57 +0500

    Initial commit

---
+++ app/src/main/java/com/example/whatapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.whatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Whatsapp");
+    }
+}
